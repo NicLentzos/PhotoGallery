5ac787535eedd9f7af72eedf295f31f568fc30a4
Nic Lentzos <nicholaslentzos@gmail.com>
2016-11-25 14:20:22 +0000

    p.409 - Blank screen

--- app/src/main/java/com/lentzos/nic/photogallery/PhotoGalleryActivity.java
+++ app/src/main/java/com/lentzos/nic/photogallery/PhotoGalleryActivity.java
@@ -1,13 +1,13 @@
 package com.lentzos.nic.photogallery;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-public class PhotoGalleryActivity extends AppCompatActivity {
+//Set up as singlefragmentactivity, then implement createfragment() and get it to return an instance of photogalleryfragment.
+public class PhotoGalleryActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_photo_gallery);
+    public Fragment createFragment(){
+        return PhotoGalleryFragment.newInstance();
     }
 }
