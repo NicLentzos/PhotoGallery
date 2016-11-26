2341aebc27a317ed8a3796767b7e5b0aa6de9a35
Nic Lentzos <nicholaslentzos@gmail.com>
2016-11-26 18:53:27 +0000

    Chapter 22 NerdLauncher p.422 - PhotoGallery JSON parsing code.

--- app/src/main/java/com/lentzos/nic/photogallery/FlickrFetchr.java
+++ app/src/main/java/com/lentzos/nic/photogallery/FlickrFetchr.java
@@ -1,13 +1,20 @@
 package com.lentzos.nic.photogallery;
 
 import android.net.Uri;
+import android.renderscript.RSInvalidStateException;
 import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Nic on 26/11/2016.
@@ -49,7 +56,9 @@ public class FlickrFetchr {
     //Method to build a request URL and fetch the URL contents.
     //Use uri.builder to build the complete URL for flickr api request. This method automatically escapes query strings etc for you.
     //the extras parameter url_s tells flickr to supply the URL for the small version of the picture if it is available.
-    public void fetchItems() {
+    public List<GalleryItem> fetchItems() {
+        //List of GalleryItems
+        List<GalleryItem> items = new ArrayList<>();
         try {
             String url = Uri.parse("https://api.flickr.com/services/rest/").buildUpon()
                     .appendQueryParameter("method", "flickr.photos.getRecent")
@@ -60,8 +69,41 @@ public class FlickrFetchr {
                     .build().toString();
             String jsonString = getUrlString(url);
             Log.i(TAG, "Received json: " + jsonString);
+            //Parse JSON text into corresponding Java objects using JSONObject() constructor.
+            JSONObject jsonBody = new JSONObject(jsonString);
+            //call parseItems() and return a list of GalleryItems.
+            parseItems(items, jsonBody);
+        } catch (JSONException je) {
+            Log.e(TAG,"Failed to parse JSON", je);
         } catch (IOException ioe) {
             Log.e(TAG, "Failed to fetch items", ioe);
+        }
+
+        return items;
+    }
+
+    //You have a JSON top level object (maps to outermost curly braces of JSON file). This contains a nested JSON
+    //object named photos. Within this is a JSONArray named photo. The array contains a collection of JSON objects
+    //that represent metadata for a single photo.
+    //Need to make a GalleryItem for each photo and add it to a list.
+    private void parseItems(List<GalleryItem> items, JSONObject jsonBody) throws IOException, JSONException{
+        JSONObject photosJsonObject = jsonBody.getJSONObject("photos");
+        JSONArray photoJsonArray = photosJsonObject.getJSONArray("photo");
+
+        for (int i = 0; i< photoJsonArray.length(); i++) {
+            JSONObject photoJsonObject = photoJsonArray.getJSONObject(i);
+
+            GalleryItem item = new GalleryItem();
+            item.setId(photoJsonObject.getString("id"));
+            item.setCaption(photoJsonObject.getString("title"));
+
+            //ignore images that do not have an image URL.
+            if(!photoJsonObject.has("url_s")) {
+                continue;
+            }
+
+            item.setUrl(photoJsonObject.getString("url_s"));
+            items.add(item);
         }
     }
 }
